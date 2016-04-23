d8704b358a30f2a5936e2d86ccc6514c4bd57799
magarcia1 <magarcia1@email.arizona.edu>
2016-04-23 11:16:55 -0700

    Essential Files
    Lets start to work

---
+++ common.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * Miguel Garcia, Philip Showers, Jared
+ * 
+ */
+function heading() {
+    ?>
+
+	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+	<html xmlns="http://www.w3.org/1999/xhtml">
+		<head>
+			<title>GeoCache Search</title>
+
+		</head>
+
+		<body>
+	<?php
+}
+//Writes the footer for each page
+function footing() {
+	?>
+			<div id="footer">
+				<p>
+					Copyright &copy; 1990-2008 Internet Movie Database Inc. <br />
+					An Amazon.com company.
+				</p>
+			</div>
+
+			<div id="w3c">
+				<a href="http://validator.w3.org/check/referer"><img src="http://u.arizona.edu/~milazzom/cscv337sp14/hw7/images/w3c-xhtml.png" alt="Valid XHTML 1.1" /></a>
+				<a href="http://jigsaw.w3.org/css-validator/check/referer"><img src="http://u.arizona.edu/~milazzom/cscv337sp14/hw7/images/w3c-css.png" alt="Valid CSS" /></a>
+			</div>
+		</body>
+	</html>
+
+<?php
+}
+?>
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+	// Name: Miguel Garcia
+	// Date: 
+	// Description: 
+	
+	include 'common.php';
+	heading();
+?>
+		<form id="searchform" method="get">
+				<fieldset>
+
+				</fieldset>
+		</form>
+		
+		<div id="main">
+
+			<!-- a place to inject the results from actors.php -->
+			<div id="matches"></div>
+
+			<h1>Geo</h1>
+
+
+		</div>
+
+<?php footing(); ?>
